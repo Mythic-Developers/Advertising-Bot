1ede997a1549f1cb7696a433d1b1fa85ee5bae76
mythicbro <106801913+mythicbro@users.noreply.github.com>
2022-06-04 21:36:54 -0700

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,1 +1,16 @@
+-- advertising bot
+getgenv().chat = "test"
+getgenv().gameid = 12109643
 
+
+if syn.queue_on_teleport then
+	syn.queue_on_teleport(loadstring(game:HttpGet("https://raw.githubusercontent.com/Mythic-Developers/Advertising-Bot/main/Source.lua", true)))
+end
+
+game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents").SayMessageRequest:FireServer(getgenv().chat, "All")
+wait(5)
+game:GetService("TeleportService"):Teleport(getgenv().gameid, game.Players.LocalPlayer)
+
+if syn.queue_on_teleport then
+	syn.queue_on_teleport(loadstring(game:HttpGet("https://raw.githubusercontent.com/Mythic-Developers/Advertising-Bot/main/Source.lua", true)))
+end
